bf7d7909b52fb555adfd979d3fa7440b511553ec
Alberto Vena <alberto@codice.ch>
2011-04-26 12:57:38 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aas::Application.config.secret_token = '6d3c3712faca4e113b7bf41de44e59bbee8cb45e26e0c1595401018d105ad64d885747207ec4c39f14844b676afb4a51d1d96051a1240ea5fa228522d3ba687a'
