8f6d41c7944d69c4e362ae5e9af2fc4cfa9f377c
Alberto Vena <alberto@codice.ch>
2011-04-27 19:40:32 +0200

    Fixed typo in challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,5 @@
 class Challenge < ActiveRecord::Base
-  attr_accessible :opponent, :date, :email, :body, :locationk, :is_accepted
+  attr_accessible :opponent, :date, :email, :body, :location, :is_accepted
   
   validates_presence_of :opponent, :email, :body, :location
 end
