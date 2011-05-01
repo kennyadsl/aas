a2b433281fed89f1ac581f325ab6bba758a8dcaf
Alberto Vena <kennyadsl@gmail.com>
2011-05-01 16:13:54 +0200

    Homepage basic content added

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,5 +3,9 @@ class Player < ActiveRecord::Base
   attr_accessible :full_name, :role, :occupation, :city, :bio
   
   has_attached_file :paperclip, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  
+
+  def self.random
+    Player.offset(rand(Player.count)).first
+  end
+
 end
