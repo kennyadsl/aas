69060f3f2512d1417f47adca252df1abcdb72a0f
Alberto Vena <kennyadsl@gmail.com>
2011-05-26 15:17:37 +0200

    S3 no more supported

--- app/models/player.rb
+++ app/models/player.rb
@@ -10,19 +10,8 @@ class Player < ActiveRecord::Base
 
   before_validation :add_http
 
-  if Rails.env.production?
-    has_attached_file :paperclip,
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :storage => :s3,
-                    :bucket => "abruzzoallstars",
-                    :s3_credentials => {
-                            :access_key_id => "AKIAJFHBWGJIURYW6MUQ",
-                            :secret_access_key => "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"
-                    }
-    else
-      has_attached_file :paperclip,
+     has_attached_file :paperclip,
                     :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  end
 
   def self.random
     Player.offset(rand(Player.count)).first
