bd9a8bdb359529f4afd5d0aeba6f03ff1bd63690
Alberto Vena <kennyadsl@gmail.com>
2011-05-26 22:13:15 +0200

    Added check for migrate command

--- config/initializers/railsadmin.rb
+++ config/initializers/railsadmin.rb
@@ -1,54 +1,59 @@
 RailsAdmin.config do |config|
-  config.model Player do
-    object_label do
-      "#{bindings[:object].full_name}"
-    end
-    edit do
-      field :full_name
-      field :role
-      field :occupation
-      field :bio
-      field :who_i_am
-      field :website
-      field :is_mister
-      field :paperclip
-    end
-  end
-
-  config.model Challenge do
-    object_label do
-      "#{bindings[:object].opponent}"
-    end
-  end
-
-  config.model User do
-    visible false
-  end
-
-  config.model Event do
-    edit do
-      field :title
-      field :body, :text do
-        ckeditor true
+  begin
+    config.model Player do
+      object_label do
+        "#{bindings[:object].full_name}"
       end
-      field :opponent
-      field :date
-      field :location
-      field :is_indoor
-      field :result_home
-      field :result_guest
+      edit do
+        field :full_name
+        field :role
+        field :occupation
+        field :bio
+        field :who_i_am
+        field :website
+        field :is_mister
+        field :paperclip
+      end
     end
-    list do
-      field :title
-      field :opponent
-      field :location
-    end
-  end
 
-  config.model Picture do
-    list do
-      field :title
+    config.model Challenge do
+      object_label do
+        "#{bindings[:object].opponent}"
+      end
     end
+
+    config.model User do
+      visible false
+    end
+
+    config.model Event do
+      edit do
+        field :title
+        field :body, :text do
+          ckeditor true
+        end
+        field :opponent
+        field :date
+        field :location
+        field :is_indoor
+        field :result_home
+        field :result_guest
+      end
+      list do
+        field :title
+        field :opponent
+        field :location
+      end
+    end
+
+    config.model Picture do
+      list do
+        field :title
+      end
+    end
+
+  rescue
+    # making sure it doesn't prevent the initial rake db:migrate from working
   end
 end
 
