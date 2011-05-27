fb59c61d036659333fe4ffea21c20415e27379b1
Alberto Vena <kennyadsl@gmail.com>
2011-05-27 10:27:01 +0200

    addded rails_admin file s

--- db/schema.rb
+++ db/schema.rb
@@ -13,91 +13,90 @@
 ActiveRecord::Schema.define(:version => 20110512153941) do
 
   create_table "challenges", :force => true do |t|
-    t.string   "opponent"
-    t.datetime "date"
-    t.string   "email"
-    t.text     "body"
-    t.string   "location"
-    t.boolean  "is_accepted"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "opponent"
+    t.timestamp "date"
+    t.string    "email"
+    t.text      "body"
+    t.string    "location"
+    t.boolean   "is_accepted"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "events", :force => true do |t|
-    t.text     "body"
-    t.datetime "date"
-    t.string   "location"
-    t.string   "opponent"
-    t.boolean  "is_indoor"
-    t.integer  "result_home"
-    t.integer  "result_guest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "title"
+    t.text      "body"
+    t.timestamp "date"
+    t.string    "location"
+    t.string    "opponent"
+    t.boolean   "is_indoor"
+    t.integer   "result_home"
+    t.integer   "result_guest"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "title"
   end
 
   create_table "news", :force => true do |t|
-    t.text     "body"
-    t.string   "link"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text      "body"
+    t.string    "link"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "pictures", :force => true do |t|
-    t.integer  "event_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "paperclip_file_name"
-    t.string   "paperclip_content_type"
-    t.integer  "paperclip_file_size"
-    t.datetime "paperclip_updated_at"
-    t.string   "title"
+    t.integer   "event_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "paperclip_file_name"
+    t.string    "paperclip_content_type"
+    t.integer   "paperclip_file_size"
+    t.timestamp "paperclip_updated_at"
+    t.string    "title"
   end
 
   create_table "players", :force => true do |t|
-    t.string   "full_name"
-    t.string   "occupation"
-    t.string   "city"
-    t.string   "role"
-    t.text     "bio"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "paperclip_file_name"
-    t.string   "paperclip_content_type"
-    t.integer  "paperclip_file_size"
-    t.datetime "paperclip_updated_at"
-    t.text     "who_i_am"
-    t.string   "link"
-    t.string   "website"
-    t.boolean  "is_mister"
+    t.string    "full_name"
+    t.string    "occupation"
+    t.string    "city"
+    t.string    "role"
+    t.text      "bio"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "paperclip_file_name"
+    t.string    "paperclip_content_type"
+    t.integer   "paperclip_file_size"
+    t.timestamp "paperclip_updated_at"
+    t.text      "who_i_am"
+    t.string    "website"
+    t.boolean   "is_mister"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
-    t.string   "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "message"
+    t.string    "username"
+    t.integer   "item"
+    t.string    "table"
+    t.integer   "month"
+    t.integer   "year"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
