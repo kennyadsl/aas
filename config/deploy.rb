d1289da8dd392025f920a3d2423e9601265cad9d
Alberto Vena <kennyadsl@gmail.com>
2011-05-27 15:16:21 +0200

    upload deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,3 +78,41 @@ namespace :deploy do
 
   after "deploy:symlink", "deploy:db:replace_conf"
 end
+
+# ==============================
+# Uploads
+# ==============================
+
+namespace :uploads do
+
+  desc <<-EOD
+    Creates the upload folders unless they exist
+    and sets the proper upload permissions.
+  EOD
+  task :setup, :except => { :no_release => true } do
+    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+  end
+
+  desc <<-EOD
+    [internal] Creates the symlink to uploads shared folder
+    for the most recently deployed version.
+  EOD
+  task :symlink, :except => { :no_release => true } do
+    run "rm -rf #{release_path}/public/system"
+    run "ln -nfs #{shared_path}/public/system #{release_path}/public/system"
+  end
+
+  desc <<-EOD
+    [internal] Computes uploads directory paths
+    and registers them in Capistrano environment.
+  EOD
+  task :register_dirs do
+    set :uploads_dirs,    %w(system system/paperclips)
+    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+  end
+
+  after       "deploy:finalize_update", "uploads:symlink"
+  on :start,  "uploads:register_dirs"
+
+end
